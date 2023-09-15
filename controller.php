0c6bf81cf824bca13d82ef3cb7199714fa0cb4e5
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-15 10:02:35 +0700

    new controller

--- controller.php
+++ controller.php
@@ -8,7 +8,21 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>lutfi</title>
-    <
+    </head>
+    <body>
+    <style>collator_asort</style>
+    </body>
+    </html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.221">
+    <title>Document</title>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
         
     </body>
     </html>
