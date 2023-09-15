8c127453da007537c125d5a649124c4dd17e3865
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-15 09:54:26 +0700

    testing new role

--- controller.php
+++ controller.php
@@ -1,7 +1,17 @@
 <html lang="en">
 <head>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>lutfi</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.221">
     <title>Document</title>
 </head>
 <body>
